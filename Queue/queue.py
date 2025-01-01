93224bf7b65bf067adbe599e692e6057258041e8
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-01 15:37:50 +0530

    Create queue.py

---
+++ Queue/queue.py
@@ -0,0 +1,13 @@
+
+queue = []
+
+queue.insert(0,1)
+queue.insert(0,2)
+queue.insert(0,12)
+queue.insert(0,31)
+queue.insert(0,14)
+
+print(queue)
+
+# out put
+#[14, 31, 12, 2, 1]                # First in First out
