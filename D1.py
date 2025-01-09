b8e8d978a16804bcea00545b0a7195baacc7e0e4
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-09 21:56:13 +0530

    Add files via upload

---
+++ D1.py
@@ -0,0 +1,11 @@
+
+phone = {'iphone' : 60000 , 'redmi' : 15000, 'vivo' : 30000, 'moto' : 19000}
+
+
+print(phone.keys())
+print(phone.values())
+
+print(" Phone name  brand and prices ")
+print("===========#====*====*====#===============")
+
+print(phone)
