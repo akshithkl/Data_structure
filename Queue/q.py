142eb352a6e74f2ce3e8e3f639917261c810c615
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-12 22:59:06 +0530

    Add files via upload

---
+++ Queue/q.py
@@ -0,0 +1,15 @@
+
+queue = []
+
+queue.insert(0,1,3)
+queue.insert(0,2,2)
+queue.insert(0,12,7)
+queue.insert(0,31,3)
+queue.insert(0,14,8)
+
+print(queue)
+
+print("----------")
+queue.pop()
+
+print(queue)
