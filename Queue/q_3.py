f0e71eb22e06d052c906800c39d477c3ae59e8d5
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-15 16:18:38 +0530

    Add files via upload

---
+++ Queue/q_3.py
@@ -0,0 +1,11 @@
+queue = []
+
+queue.insert(0, 'https://www.youtbe.come')
+queue.insert(0, 'https://www.google.come')
+queue.insert(0, 'https://www.chrome.come')
+queue.insert(0, 'https://www.instagram.come')
+
+print(queue)
+
+queue.pop()
+print(queue)
