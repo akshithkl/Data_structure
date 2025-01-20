645dd7b421a5d212d1e94a519d8933dbdacfc6c5
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-20 11:43:12 +0530

    Add files via upload

---
+++ l.py
@@ -0,0 +1,2 @@
+dynamic_list = [1, 2, 3]
+dynamic_list.append(4)  # Add an element
---
+++ l_2.py
@@ -0,0 +1,20 @@
+my_list = [1, 2, 3, 4, 'apple']
+print(f" Original list : {my_list}")
+
+print()
+
+my_list.insert(2,'banana')
+print(f"After insert the Banana : {my_list}")
+print()
+
+my_list.remove("apple") # for delete the element
+print(f" after remove the apple:{my_list}")
+print()
+
+my_list.pop(0) # for delete the index 
+print(f" Aftre pop the index : {my_list}")
+print()
+
+my_list.append(51) #which is the element to be added to the end of the list
+print(f" After append the element : {my_list}")
+
