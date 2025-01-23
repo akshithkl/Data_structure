e411969935547493260da9aaa482eb63629b4092
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-23 20:38:04 +0530

    Add files via upload

---
+++ Set/s2.py
@@ -0,0 +1,15 @@
+a = set((1, 4, 8, 6))
+print(a)
+print()
+
+b = set([1, 4, 2, 77])
+print(b)
+print()
+
+c = set({1, 6, 3, 6})
+print(c)
+print()
+
+d = {4, 2, 7, 3}
+d.remove(4)
+print(d)
---
+++ Set/s3.py
@@ -0,0 +1,5 @@
+a = set((1, 4, 8, 6)) # a = set(1, 4, 8, 6) , why because set take argument like tuple list  
+print(a)
+print()
+
+print(a[0])
