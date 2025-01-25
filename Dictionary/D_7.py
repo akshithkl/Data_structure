4632c16c2f2351b4fb9b6caec4b736406d00b773
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-01-25 10:18:30 +0530

    Add files via upload

---
+++ Dictionary/D_7.py
@@ -0,0 +1,4 @@
+word = ['a', 'b', 'c', 'd']
+
+result = {i: j for i, j in enumerate(word)}
+print(result)
---
+++ Dictionary/D_9.py
@@ -0,0 +1,4 @@
+my_dict = {"a": 1, "b": 2, "c": 3}
+
+for index, key in enumerate(my_dict):
+    print(f"index: {index}, Key: {key}") 
