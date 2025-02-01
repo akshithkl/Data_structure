de2a451d1dc21d9eb97b5f9c81648e1718072a10
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-02-01 11:05:24 +0530

    Add files via upload

---
+++ Linked_list/link_2.py
@@ -0,0 +1,19 @@
+
+# 1. creating a node in singly linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+#n = Node(3)
+#print(n.data)
+#print(n.next)
+
+# 2. creating a class of singly linked list
+
+class singly:
+    def __init__(self):
+        self.head = None
+        #print("hello")
+
+#sll = singly()
