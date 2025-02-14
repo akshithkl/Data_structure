5985ca8bd98f49a97bb9365465e85c4164872fa9
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-02-14 11:47:25 +0530

    Update D_10.py

--- Dictionary/D_10.py
+++ Dictionary/D_10.py
@@ -12,3 +12,11 @@ for i, v in enumerate(nam):
 my_dict['a'] -= 1
 
 print(my_dict)
+
+'''
+Why no KeyError?
+Counter is a subclass of dict, but it automatically initializes missing keys to 0 when you increment them.
+When window_counter[c] += 1 runs for the first time, Counter sees that c is not in the dictionary and assumes window_counter[c] = 0, then increments it to 1.
+This built-in behavior prevents a KeyError.
+
+'''
