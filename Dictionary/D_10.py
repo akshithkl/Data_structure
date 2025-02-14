b92222f33b8fc77462b18b59555e6a6ddbf97b57
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-02-14 11:46:02 +0530

    Add files via upload

---
+++ Dictionary/D_10.py
@@ -0,0 +1,14 @@
+from collections import Counter
+nam ='akshith'
+my_dict = Counter()
+
+for i, v in enumerate(nam):
+  
+    my_dict[v] += 1
+
+    
+        
+
+my_dict['a'] -= 1
+
+print(my_dict)
