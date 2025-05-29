7519b2b39f9153931399f26bf552ba142b074ecc
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-05-29 08:33:32 +0530

    Add files via upload

---
+++ Dictionary/D_15.py
@@ -0,0 +1,4 @@
+person = {"name": "Akshi", "age": 21}
+print(person.get("name"))         # Output: Akshi
+print(person.get("gender"))       # Output: None
+print(person.get("gender", "N/A"))  # Output: N/A
